cf9f28429a977bbb7a4889805f9961fb566b582b
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-04-13 17:41:12 +0500

    Create __main__.py

---
+++ colorboy/__main__.py
@@ -0,0 +1,11 @@
+from colorama import Fore
+def main():
+  while True:
+    print(Fore.BLUE+"█",end="")
+    print(Fore.BLACK+"█",end="")
+    print(Fore.RED+"█",end="")
+    print(Fore.GREEN+"█",end="")
+    print(Fore.YELLOW+"█",end="")
+    print(Fore.MAGENTA+"█",end="")
+    print(Fore.CYAN+"█",end="")
+    print(Fore.WHITE+"",end="")
